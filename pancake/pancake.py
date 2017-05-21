e42b4594384d766acd3f33b4cb0efa9bbc36ee4c
Anthony Robertson <anthony@Anthonys-MacBook-Pro.local>
2017-05-21 00:54:35 -0400

    Adding a fully functioning flip function

--- pancake/pancake.py
+++ pancake/pancake.py
@@ -8,11 +8,34 @@ def ginger_sorts():
     return
 
 def flip(number):
+    # Global Variables
+    global stack, numPancakes, numFlips, numFlipped
+
+    before = stack[0 : number + 1]
+
+    # Flip the values of the pancakes to be flipped
+    for i in range(0, number + 1):
+        if before[i] == 0:
+            before[i] = 1
+        else:
+            before[i] = 0
+
+    # Reverse the order of the pancakes to be flipped
+    # and place back on the the stack
+    j = number
+    for i in range(0, number + 1):
+        stack[i] = before[j]
+        j -= 1
+
+    numFlipped += number + 1
+    numFlips += 1
     return
 
 def print_report(name):
+    # Global Variables
     global stack, numPancakes, visual
     global numFlips, avgFlipSize, numFlipped
+
     avgFlipSize = numFlipped / numFlips
     print('-----------------------')
     print(' Results - ', name)
@@ -23,12 +46,16 @@ def print_report(name):
     return
 
 def reset():
+    # Global Variables
     global numFlips, avgFlipSize, numFlipped
+
     numFlips = numFlipped = avgFlipSize = 0
     return
 
 def init():
+    # Global Variables
     global stack, numPancakes, numRuns, visual, fileIn
+
     stack = []
     fileIn = open('input.txt', 'r')
     numPancakes = int(fileIn.readline())
@@ -42,6 +69,8 @@ def init():
 def main():
     init()
     reset()
+    ginger_sorts()
+    print_report('Ginger')
     return
 
 if __name__ == '__main__':
