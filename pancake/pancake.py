bb16d7a2d8ca344c3fe688f93da963fe96c20169
Anthony Robertson <anthony@Anthonys-MacBook-Pro.local>
2017-05-21 01:18:14 -0400

    Adding gingers sort as well as some comments and minor refactors

--- pancake/pancake.py
+++ pancake/pancake.py
@@ -4,17 +4,30 @@ from random import randint
 def joseph_sorts():
     return
 
+# This sort looks at the top pancake and makes sure its upside down
+# The stack is then flipped to "Thow the top pancake to the bottom"
+# The process is repeated flipping 1 pancake less each time until
+# the entire stack is flipped
 def ginger_sorts():
+    # Global Variables
+    global stack, numPancakes
+
+    lastIndex = numPancakes - 1
+    for i in range(0, lastIndex + 1):
+        if stack[0] == 1:
+            flip(0)
+        flip(lastIndex - i)
     return
 
-def flip(number):
+# Function to flip a stack of 1's and 0's from 0 through a given index
+def flip(index):
     # Global Variables
     global stack, numPancakes, numFlips, numFlipped
 
-    before = stack[0 : number + 1]
+    before = stack[0 : index + 1]
 
     # Flip the values of the pancakes to be flipped
-    for i in range(0, number + 1):
+    for i in range(0, index + 1):
         if before[i] == 0:
             before[i] = 1
         else:
@@ -22,15 +35,16 @@ def flip(number):
 
     # Reverse the order of the pancakes to be flipped
     # and place back on the the stack
-    j = number
-    for i in range(0, number + 1):
+    j = index
+    for i in range(0, index + 1):
         stack[i] = before[j]
         j -= 1
 
-    numFlipped += number + 1
+    numFlipped += index + 1
     numFlips += 1
     return
 
+# Prints tracked stats about the last run algorithm
 def print_report(name):
     # Global Variables
     global stack, numPancakes, visual
@@ -45,6 +59,7 @@ def print_report(name):
     print('Average Pancakes / Flip: ', avgFlipSize)
     return
 
+# Resets the stat tracking variables
 def reset():
     # Global Variables
     global numFlips, avgFlipSize, numFlipped
@@ -52,6 +67,15 @@ def reset():
     numFlips = numFlipped = avgFlipSize = 0
     return
 
+# Set up a randomized stack of 0's and 1's
+def randomize_stack():
+    global stack, numPancakes
+    for i in range(0, numPancakes):
+        stack.append(randint(0, 1))
+    return
+
+
+# Initializing data from the input file
 def init():
     # Global Variables
     global stack, numPancakes, numRuns, visual, fileIn
@@ -62,9 +86,8 @@ def init():
     numRuns = int(fileIn.readline())
     visual = int(fileIn.readline())
     fileIn.close()
-    for i in range(0,numPancakes):
-        stack.append(randint(0,1))
-    return
+    randomize_stack()
+
 
 def main():
     init()
