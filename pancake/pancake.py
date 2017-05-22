c874303b2c721d5d9d37114208ca47f761378c4f
Anthony Robertson <arobertson1796@gmail.com>
2017-05-21 23:51:41 -0400

    Adding time stats and multiple run capabilities Also improved output to reflect multiple runs

--- pancake/pancake.py
+++ pancake/pancake.py
@@ -1,5 +1,7 @@
 #!/usr/local/bin/python3
 from random import randint
+import time
+
 
 def joseph_sorts():
     return
@@ -50,7 +52,7 @@ def ginger_sorts():
 # Function to flip a stack of 1's and 0's from 0 through a given index
 def flip(index):
     # Global Variables
-    global stack, numPancakes, numFlips, numFlipped
+    global stack, numPancakes, totalFlips, totalFlipped
 
     before = stack[0 : index + 1]
 
@@ -68,77 +70,101 @@ def flip(index):
         stack[i] = before[j]
         j -= 1
 
-    numFlipped += index + 1
-    numFlips += 1
+    totalFlipped += index + 1
+    totalFlips += 1
     return
 
 # Prints tracked stats about the last run algorithm
 def print_report(name):
     # Global Variables
-    global stack, numPancakes, visual
-    global numFlips, avgFlipSize, numFlipped
+    global totalFlips, totalFlipped, totalTime, numRuns
 
-    avgFlipSize = numFlipped / numFlips
+    avgFlipSize = totalFlipped / totalFlips
+    avgNumFlips = totalFlips / numRuns
+    avgNumFlipped = totalFlipped / numRuns
+    avgTime = totalTime / numRuns
     print ('-----------------------')
     print ('     ', name)
     print ('-----------------------')
-    print ('Total Flips: ', numFlips)
-    print ('Total Pancakes Flipped: ', numFlipped)
-    print ('Average Pancakes / Flip: ', avgFlipSize)
+    print ('Total Flips: ', totalFlips)
+    print ('Average # of flips: %.2f' % (avgNumFlips))
+    print ('Average # of Pancakes / Flip: %.2f' % (avgFlipSize))
+    print ('Average # of Pancakes Flipped: %.2f' % (avgNumFlipped))
+    print ('Total time taken: %.5f' % (totalTime))
+    print ('Average time taken: %.5f' % (avgTime))
     return
 
 # Resets the stat tracking variables
 def reset():
     # Global Variables
-    global numFlips, avgFlipSize, numFlipped, stack
-    global originalStack
+    global totalFlips, totalFlipped, totalTime
 
-    numFlips = numFlipped = avgFlipSize = 0
-    stack = originalStack[:]
+    totalFlips = totalFlipped = 0
+    totalTime = 0
     return
 
 # Set up a randomized stack of 0's and 1's
-def randomize_stack():
-    global stack, numPancakes
-    for i in range(0, numPancakes):
-        stack.append(randint(0, 1))
+def create_stacks():
+    global allStacks, stack, numPancakes, numRuns
+    allStacks = []
+    for i in range(0, numRuns):
+        stack = []
+        for i in range(0, numPancakes):
+            stack.append(randint(0, 1))
+        allStacks.append(stack[:])
     return
-
 
 # Initializing data from the input file
 def init():
     # Global Variables
-    global stack, numPancakes, numRuns, visual, fileIn
-    global originalStack
+    global numPancakes, numRuns, fileIn
 
-    stack = []
     fileIn = open('input.txt', 'r')
     numPancakes = int(fileIn.readline())
     numRuns = int(fileIn.readline())
-    visual = int(fileIn.readline())
     fileIn.close()
-    randomize_stack()
-    originalStack = stack[:]
+    create_stacks()
 
+def run_algorithms():
+    # Global varaibles
+    global allStacks, stack, numRuns, totalTime
 
+    reset()
+    for i in range(0, numRuns):
+        stack = allStacks[i][:]
+        startTime = time.time()
+        ginger_sorts()
+        totalTime += time.time() - startTime
+    print_report('Ginger')
+
+    reset()
+    for i in range(0, numRuns):
+        stack = allStacks[i][:]
+        startTime = time.time()
+        anthony_sorts(0)
+        totalTime += time.time() - startTime
+    print_report('Anthony DUMB')
+
+    reset()
+    for i in range(0, numRuns):
+        stack = allStacks[i][:]
+        startTime = time.time()
+        anthony_sorts(1)
+        totalTime += time.time() - startTime
+    print_report('Anthony SMART')
+
+    return
 
 def main():
     # Global variables
-    global numPancakes
+    global numPancakes, numRuns
     init()
-    reset()
     print ('========================================')
     print ('                Results')
     print ('Number of Pancakes: ', numPancakes)
+    print ('Number of Runs: ', numRuns)
     print ('========================================')
-    ginger_sorts()
-    print_report('Ginger')
-    reset()
-    anthony_sorts(0)
-    print_report('Anthony DUMB')
-    reset()
-    anthony_sorts(1)
-    print_report('Anthony SMART')
+    run_algorithms()
     return
 
 if __name__ == '__main__':
